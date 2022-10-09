2ce00b25393a38e7c46391aa7cf452b1b2eccc37
d.stukov <d.stukov@skbkontur.ru>
2022-10-09 11:01:38 +0500

    review fix

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,3 +1,3 @@
-export function IsNullOrWhiteSpace(value: string) {
-  return value === null || value.trim() === '';
+export function IsNullOrWhiteSpace(value?: string) {
+  return !value || !value.trim();
 }
