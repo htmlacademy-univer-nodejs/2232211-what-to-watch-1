8133556ecb90e0c2c9074050c4136dbbfd794c46
d.stukov <d.stukov@skbkontur.ru>
2022-10-23 12:08:43 +0500

    Add new env variables and get db connection uri wip: 'uri' is declared but its value is never read.

--- src/app/application.ts
+++ src/app/application.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { Component } from '../types/component.js';
 import { ILog } from '../common/loggers/logger.interface';
 import { IConfig } from '../common/config/config.interface';
+import { getDBConnectionURI } from '../utils/db.js';
 
 @injectable()
 export default class Application {
@@ -13,5 +14,12 @@ export default class Application {
   async init() {
     this.logger.info('Application initialized.');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
+    const uri = getDBConnectionURI(this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME')
+    );
   }
 }
---
+++ src/utils/db.ts
@@ -0,0 +1,8 @@
+export function getDBConnectionURI(
+  username: string,
+  password: string,
+  host: string,
+  port: number,
+  dbName: string): string {
+  return `mongodb://${username}:${password}@${host}:${port}/${dbName}?authSource=admin`;
+}
