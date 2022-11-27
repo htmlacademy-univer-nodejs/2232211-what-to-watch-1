ad1ec8d50884c61259720d440279fee7f0ee2672
d.stukov <d.stukov@skbkontur.ru>
2022-11-27 09:21:05 +0500

    Start express app on localhost port from config

--- src/app/application.ts
+++ src/app/application.ts
@@ -4,14 +4,19 @@ import { ILog } from '../common/loggers/logger.interface';
 import { IConfig } from '../common/config/config.interface';
 import { getDBConnectionURIFromConfig } from '../utils/db.js';
 import { IDatabase } from '../common/db-client/database.interface.js';
+import express, { Express } from 'express';
 
 @injectable()
 export default class Application {
+  private expressApp: Express;
+
   constructor(
     @inject(Component.ILog) private logger: ILog,
     @inject(Component.IConfig) private config: IConfig,
     @inject(Component.IDatabase) private dbClient: IDatabase
-  ) {}
+  ) {
+    this.expressApp = express();
+  }
 
   async init() {
     this.logger.info('Application initialized.');
@@ -20,5 +25,10 @@ export default class Application {
     const uri = getDBConnectionURIFromConfig(this.config);
 
     await this.dbClient.connect(uri);
+
+    const port = this.config.get('PORT');
+    this.expressApp.listen(port, () => {
+      this.logger.info(`Server started on http://localhost:${port}`);
+    });
   }
 }
