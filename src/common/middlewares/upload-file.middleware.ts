671172cb50753c2c9d2a4be2f6ccfe59717eb859
d.stukov <d.stukov@skbkontur.ru>
2022-12-04 13:45:27 +0500

    Add and use UploadFileMiddleware

---
+++ src/common/middlewares/upload-file.middleware.ts
@@ -0,0 +1,28 @@
+import { IMiddleware } from './middleware.interface.js';
+import { NextFunction, Request, Response } from 'express';
+import multer, {diskStorage} from 'multer';
+import mime from 'mime';
+import { nanoid } from 'nanoid';
+
+export class UploadFileMiddleware implements IMiddleware {
+  constructor(
+    private uploadDirectory: string,
+    private fieldName: string,
+  ) {}
+
+  public async execute(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const storage = diskStorage({
+      destination: this.uploadDirectory,
+      filename: (_req, file, callback) => {
+        const extension = mime.getExtension(file.mimetype);
+        const filename = nanoid();
+        callback(null, `${filename}.${extension}`);
+      }
+    });
+
+    const uploadSingleFileMiddleware = multer({storage})
+      .single(this.fieldName);
+
+    uploadSingleFileMiddleware(req, res, next);
+  }
+}
