982cd68b11add7efcdb8ee3144577203e5cd636e
d.stukov <d.stukov@skbkontur.ru>
2023-01-14 22:37:56 +0500

    verifyUser for user service

--- src/modules/user/user-service.interface.ts
+++ src/modules/user/user-service.interface.ts
@@ -2,6 +2,7 @@ import { DocumentType } from '@typegoose/typegoose';
 import CreateUserDto from './dto/create-user.js';
 import { UserEntity } from './user.entity.js';
 import { MovieEntity } from '../movie/movie.entity.js';
+import LoginUserDto from './dto/login-user.js';
 
 export interface IUserService {
   create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
@@ -10,4 +11,5 @@ export interface IUserService {
   findFavorite(userId: string): Promise<DocumentType<MovieEntity>[]>;
   addFavorite(movieId: string, userId: string): Promise<void | null>;
   deleteFavorite(movieId: string, userId: string): Promise<void | null>;
+  verifyUser(dto: LoginUserDto, salt: string): Promise<DocumentType<UserEntity> | null>;
 }
