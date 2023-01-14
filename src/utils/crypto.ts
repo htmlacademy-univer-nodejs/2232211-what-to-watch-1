debd6125b9acc7e714d1df9a9abebc2c2c94d870
d.stukov <d.stukov@skbkontur.ru>
2023-01-14 22:50:07 +0500

    createJWT function

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,6 +1,14 @@
 import * as crypto from 'crypto';
+import * as jose from 'jose';
 
 export const createSHA256 = (line: string, salt: string): string => crypto
   .createHmac('sha256', salt)
   .update(line)
   .digest('hex');
+
+export const createJWT = async (algorithm: string, jwtSecret: string, payload: object): Promise<string> =>
+  new jose.SignJWT({...payload})
+    .setProtectedHeader({ alg: algorithm})
+    .setIssuedAt()
+    .setExpirationTime('2d')
+    .sign(crypto.createSecretKey(jwtSecret, 'utf-8'));
