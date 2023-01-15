3e7be05ff5925e86eea1b1a2f6599f1e0d8ad275
d.stukov <d.stukov@skbkontur.ru>
2023-01-15 14:10:41 +0500

    transformObject function

--- src/utils/common-functions.ts
+++ src/utils/common-functions.ts
@@ -2,6 +2,8 @@ import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { ValidationError } from 'class-validator';
 import { ValidationErrorField } from '../types/validation-error-field.type.js';
 import { ServiceError } from '../types/service-error.enum.js';
+import { UnknownObject } from '../types/unknown-object.type.js';
+import { DEFAULT_STATIC_IMAGES } from '../app/application.constant.js';
 
 export const fillDTO = <T, V>(someDto: ClassConstructor<T>, plainObject: V) =>
   plainToInstance(someDto, plainObject, {excludeExtraneousValues: true});
@@ -20,3 +22,28 @@ export const transformErrors = (errors: ValidationError[]): ValidationErrorField
   }));
 
 export const getFullServerPath = (host: string, port: number) => `http://${host}:${port}`;
+
+const isObject = (value: unknown) => typeof value === 'object' && value !== null;
+
+export const transformProperty = (
+  property: string,
+  someObject: UnknownObject,
+  transformFn: (object: UnknownObject) => void
+) => {
+  Object.keys(someObject)
+    .forEach((key) => {
+      if (key === property) {
+        transformFn(someObject);
+      } else if (isObject(someObject[key])) {
+        transformProperty(property, someObject[key] as UnknownObject, transformFn);
+      }
+    });
+};
+
+export const transformObject = (properties: string[], staticPath: string, uploadPath: string, data:UnknownObject) => {
+  properties
+    .forEach((property) => transformProperty(property, data, (target: UnknownObject) => {
+      const rootPath = DEFAULT_STATIC_IMAGES.includes(target[property] as string) ? staticPath : uploadPath;
+      target[property] = `${rootPath}/${target[property]}`;
+    }));
+};
